3cf201a9c477d83b211bca5cfa6272f21a9978b0
David Cuero <W5845542@student.hccs.edu>
2020-07-01 09:31:35 -0500

    Add Name and Avatar to Users, Add New users to Database

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import "firebase/database";
 import "firebase/storage";
 
 var config = {
-  apiKey: "AIzaSyAyUsBLsx6P9eT4Y4QYiMcQp5AkEYwH0n8",
+  apiKey: "AIzaSyCREQMYIbAYKscrS_JYvWP4KpMsF_jI5Bg",
   authDomain: "react-slack-clone.firebaseapp.com",
   databaseURL: "https://react-slack-clone.firebaseio.com",
   projectId: "react-slack-clone",
