48b69438837164da7848e89f42822c4ff93bf5d0
David Cuero <W5845542@student.hccs.edu>
2020-07-01 02:05:29 -0500

    Update register.js

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -42,8 +42,8 @@ class Register extends React.Component {
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
-            <Icon name="puzzle piece" color="orange" />
+          <Header as="h2" icon color="blue" textAlign="center">
+            <Icon name="puzzle piece" color="blue" />
             Register for DevChat
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
@@ -92,7 +92,7 @@ class Register extends React.Component {
                 type="password"
               />
 
-              <Button color="orange" fluid size="large">
+              <Button color="blue" fluid size="large">
                 Submit
               </Button>
             </Segment>
