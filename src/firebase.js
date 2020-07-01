38ba1e4e40f0fb45d9614dfb169ceff31b952634
David Cuero <W5845542@student.hccs.edu>
2020-07-01 02:17:34 -0500

    Clear Errors upon Registration, Show loading state

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import "firebase/database";
 import "firebase/storage";
 
 var config = {
-  apiKey: "AIzaSyCREQMYIbAYKscrS_JYvWP4KpMsF_jI5Bg",
+  apiKey: "AIzaSyAyUsBLsx6P9eT4Y4QYiMcQp5AkEYwH0n8",
   authDomain: "react-slack-clone.firebaseapp.com",
   databaseURL: "https://react-slack-clone.firebaseio.com",
   projectId: "react-slack-clone",
